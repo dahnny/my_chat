c67ff5a6fba6c96682f57d070be15f04f45a0c0d
Daniel Ogbuti <danielogbuti@gmail.com>
2020-05-30 23:20:24 +0100

    native adjustments

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -5,7 +6,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:my_chat/widgets/auth/chat/messages.dart';
 import 'package:my_chat/widgets/auth/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,7 +57,27 @@ class ChatScreen extends StatelessWidget {
           ],
         ),
       ),
-
     );
+  }
+
+  @override
+  void initState() {
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+    fbm.configure(
+      onMessage: (msg) {
+        print(msg);
+        return;
+      },
+      onLaunch: (msg) {
+        print(msg);
+        return;
+      },
+      onResume: (msg) {
+        print(msg);
+        return;
+      },
+    );
+    super.initState();
   }
 }
